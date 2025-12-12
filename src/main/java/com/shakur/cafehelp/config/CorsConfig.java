a7e30a1054404f7889b0d2937e8ca61cc30cbf10
Shakur <117600671+ShakurDmitrii@users.noreply.github.com>
2025-12-13 01:28:21 +0300

    delete FavoriteProducts and add isFavorite in Product

--- src/main/java/com/shakur/cafehelp/config/CorsConfig.java
+++ src/main/java/com/shakur/cafehelp/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")  // Все пути с /api/
                 .allowedOrigins("http://localhost:3000")  // React dev сервер
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
